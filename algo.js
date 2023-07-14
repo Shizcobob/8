0194b6951e6d3e229068cafb1c0d522e9915af83
Shizcobob <kyle.korinek@outlook.com>
2023-07-14 11:58:07 -0400

    submission

---
+++ algo.js
@@ -0,0 +1,107 @@
+/* 
+  Given an unsorted non-empty array of integers and int k, return the k most frequent elements (in any order)
+  You can assume there is always a valid solution
+
+  These example inputs are sorted for readability, but the input is NOT guaranteed to be sorted and the output does NOT need to be in any specific order
+
+  Hard Bonus: make it O(n) time
+*/
+
+const numbers1 = [1, 1, 1, 2, 2, 3];
+const k1 = 2;
+const expected1 = [1, 2];
+// Explanation: return the two most frequent elements, 1 and 2 are the two most frequent elements
+
+const numbers2 = [0, 0, 0, 2, 2, 3];
+const k2 = 1;
+const expected2 = [0];
+// Explanation: k being 1 means return the single most frequent element
+
+// 6 occurs 6 times, 3 occurs 3 times, 2 occurs 2 times, 1 occurs 1 time.
+const numbers3 = [1, 6, 3, 3, 6, 6, 3, 6, 2, 2, 6, 6];
+const k3 = 3;
+const expected3 = [6, 3, 2];
+
+/**
+ * Returns the k most frequently occurring numbers from the given unordered
+ * numbers.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<number>} numbers Unordered.
+ * @param {number} k Represents the amount of numbers that should be returned.
+ * @returns {Array<number>} The k most frequently occurring numbers.
+ */
+function kMostFrequent(numbers, k) {
+    let count = 0;
+    let blank = []
+    for(let i =0; i < numbers.length; i++){ 
+        if(blank.includes(numbers[i]) !== numbers[i] ){
+            blank.push(numbers[i])
+        }
+        if(blank.includes(numbers[i]) === numbers[i]){
+            let count
+        }
+    }
+}
+/* 
+  Given an unsorted non-empty array of integers and int k, return the k most frequent elements (in any order)
+  You can assume there is always a valid solution
+
+  These example inputs are sorted for readability, but the input is NOT guaranteed to be sorted and the output does NOT need to be in any specific order
+
+  Hard Bonus: make it O(n) time
+*/
+
+const numbers1 = [1, 1, 1, 2, 2, 3];
+const k1 = 2;
+const expected1 = [1, 2];
+
+const numbers2 = [0, 0, 0, 2, 2, 3];
+const k2 = 1;
+const expected2 = [0];
+
+const numbers3 = [1, 6, 3, 3, 6, 6, 3, 6, 2, 2, 6, 6];
+const k3 = 3;
+const expected3 = [6, 3, 2];
+
+/
+ 
+// Returns the k most frequently occurring numbers from the given unordered
+// numbers.
+// Time: O(?).
+// Space: O(?).
+// @param {Array<number>} numbers Unordered.
+// @param {number} k Represents the amount of numbers that should be returned.
+// @returns {Array<number>} The k most frequently occurring numbers.
+// */
+
+function kMostFrequent(numbers, k) {
+    const frequencyTable = {};
+    for (const num of numbers) {
+        if (frequencyTable[num] === undefined) {
+            frequencyTable[num] = 0
+            console.log(frequencyTable);
+                    console.log("__`___");
+        }
+        frequencyTable[num]++;
+        // console.log(frequencyTable);
+    }
+    // Step 2: Create an array of unique numbers
+    const serbsNumbers = Object.keys(frequencyTable).map(Number);
+    //   console.log(serbsNumbers);
+
+    // Step 3: Sort the unique numbers array based on frequencies
+    serbsNumbers.sort((a, b) => frequencyTable[b] - frequencyTable[a]);
+
+    // Step 4: Return the k most frequent elements
+    return serbsNumbers.slice(0, k);
+
+}
+console.log(kMostFrequent(numbers1, k1));
+console.log("**")
+console.log(kMostFrequent(numbers2, k2));
+console.log("**")
+console.log(kMostFrequent(numbers3, k3));
+
+
+/*****************************************************************************/
